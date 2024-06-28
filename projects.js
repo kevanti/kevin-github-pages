1b32e80438b69a7c214593f0ab82ec1ed49f3689
Kevin Álvarez <kevin1alvarez1.69@gmail.com>
2024-06-28 10:43:21 +0200

    añadiendo el contenido de los diferents section

--- projects.js
+++ projects.js
@@ -1,21 +1,14 @@
-let allProjects = document.getElementById("workAll");
-
 //Menu
-let noReponsive = document.getElementById("noReponsive");
 let todos = document.getElementById("todos");
+let noReponsive = document.getElementById("noReponsive");
+let reponsive = document.getElementById("Reponsive");
+let game = document.getElementById("Game");
 
-let sectionNoReponsive = document.getElementById("sectionNoReponsive")
-
-
-//No reponsive
-noReponsive.addEventListener("click", (event)=>{
-    event.preventDefault();
-
-    sectionNoReponsive.classList.remove("hidden");
-    allProjects.classList.add("hidden");
-    noReponsive.style.color= "#0cb7f2";
-    todos.style.color= '#44566C';
-});
+//Sections
+let allProjects = document.getElementById("workAll");
+let sectionNoReponsive = document.getElementById("sectionNoReponsive");
+let sectionReponsive = document.getElementById("sectionReponsive");
+let sectionGame = document.getElementById("sectionGame");
 
 //todos
 todos.addEventListener("click", (event)=>{
@@ -23,6 +16,50 @@ todos.addEventListener("click", (event)=>{
 
     sectionNoReponsive.classList.add("hidden");
     allProjects.classList.remove("hidden");
-    noReponsive.style.color= '#44566C';
+
     todos.style.color = "#0cb7f2";
+    noReponsive.style.color= '#44566C';
+    reponsive.style.color = "#44566C";
+    game.style.color = "#44566C";
 });
+
+//No reponsive
+noReponsive.addEventListener("click", (event)=>{
+    event.preventDefault();
+
+    sectionNoReponsive.classList.remove("hidden");
+    allProjects.classList.add("hidden");
+
+    noReponsive.style.color= "#0cb7f2";
+    todos.style.color= '#44566C';
+    game.style.color = "#44566C";
+});
+
+//Responsive
+reponsive.addEventListener("click", (event)=>{
+    event.preventDefault();
+
+    allProjects.classList.add("hidden");
+    sectionNoReponsive.classList.add("hidden");
+    sectionReponsive.classList.remove("hidden");
+
+    reponsive.style.color = "#0cb7f2";
+    todos.style.color = "#44566C";
+    noReponsive.style.color = "#44566C";
+    game.style.color = "#44566C";
+
+})
+game.addEventListener("click", (event)=>{
+    event.preventDefault();
+
+    allProjects.classList.add("hidden");
+    sectionNoReponsive.classList.add("hidden");
+    sectionReponsive.classList.add("hidden");
+    sectionGame.classList.remove("hidden");
+
+    todos.style.color = "#44566C";
+    noReponsive.style.color = "#44566C";
+    reponsive.style.color = "#44566C";
+    game.style.color = "#0cb7f2";
+    
+})
