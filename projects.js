2e04312e03b047387046687e131e96fb58fe148d
Kevin Álvarez <kevin1alvarez1.69@gmail.com>
2024-06-29 21:45:19 +0200

    añadiendo los flyers

--- projects.js
+++ projects.js
@@ -3,12 +3,14 @@ let todos = document.getElementById("todos");
 let noReponsive = document.getElementById("noReponsive");
 let reponsive = document.getElementById("Reponsive");
 let game = document.getElementById("Game");
+let flyers = document.getElementById("Flyer");
 
 //Sections
 let allProjects = document.getElementById("workAll");
 let sectionNoReponsive = document.getElementById("sectionNoReponsive");
 let sectionReponsive = document.getElementById("sectionReponsive");
 let sectionGame = document.getElementById("sectionGame");
+let SectionFlyers = document.getElementById("sectionFlyers");
 
 //todos
 todos.addEventListener("click", (event)=>{
@@ -17,11 +19,13 @@ todos.addEventListener("click", (event)=>{
     sectionNoReponsive.classList.add("hidden");
     allProjects.classList.remove("hidden");
     sectionGame.classList.add("hidden");
+    SectionFlyers.classList.add("hidden");
 
     todos.style.color = "#0cb7f2";
     noReponsive.style.color= '#44566C';
     reponsive.style.color = "#44566C";
     game.style.color = "#44566C";
+    flyers.style.color = "#44566C";
 });
 
 //No reponsive
@@ -32,11 +36,13 @@ noReponsive.addEventListener("click", (event)=>{
     allProjects.classList.add("hidden");
     sectionReponsive.classList.add("hidden");
     sectionGame.classList.add("hidden");
+    SectionFlyers.classList.add("hidden");
 
     todos.style.color= '#44566C';
     noReponsive.style.color= "#0cb7f2";
     reponsive.style.color = "#44566C";
     game.style.color = "#44566C";
+    flyers.style.color = "#44566C";
 });
 
 //Responsive
@@ -47,11 +53,13 @@ reponsive.addEventListener("click", (event)=>{
     sectionNoReponsive.classList.add("hidden");
     sectionReponsive.classList.remove("hidden");
     sectionGame.classList.add("hidden");
+    SectionFlyers.classList.add("hidden");
 
     reponsive.style.color = "#0cb7f2";
     todos.style.color = "#44566C";
     noReponsive.style.color = "#44566C";
     game.style.color = "#44566C";
+    flyers.style.color = "#44566C";
 
 })
 game.addEventListener("click", (event)=>{
@@ -61,10 +69,29 @@ game.addEventListener("click", (event)=>{
     sectionNoReponsive.classList.add("hidden");
     sectionReponsive.classList.add("hidden");
     sectionGame.classList.remove("hidden");
+    SectionFlyers.classList.add("hidden");
 
     todos.style.color = "#44566C";
     noReponsive.style.color = "#44566C";
     reponsive.style.color = "#44566C";
     game.style.color = "#0cb7f2";
+    flyers.style.color = "#44566C";
+    
+})
+
+flyers.addEventListener("click", (event)=>{
+    event.preventDefault();
+
+    allProjects.classList.add("hidden");
+    sectionNoReponsive.classList.add("hidden");
+    sectionReponsive.classList.add("hidden");
+    sectionGame.classList.add("hidden");
+    SectionFlyers.classList.remove("hidden");
+
+    todos.style.color = "#44566C";
+    noReponsive.style.color = "#44566C";
+    reponsive.style.color = "#44566C";
+    game.style.color = "#44566C";
+    flyers.style.color = "#0cb7f2";
     
 })
